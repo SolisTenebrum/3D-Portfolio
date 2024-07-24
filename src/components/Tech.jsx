c7e03aafcca5151d8489f1b6a8f5b76d1d065235
SolisTenebrum <solistenebrum@icloud.com>
2024-07-24 23:20:11 +0500

    added lazy loading for BallCanvas

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,23 @@
-import { BallCanvas } from "./canvas"
-import { SectionWrapper } from "../hoc"
-import { technologies } from "../constants"
+import React, { lazy, Suspense } from "react";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+import CanvasLoader from "./Loader";
 
+const BallCanvas = lazy(() => import("./canvas/Ball"));
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => {
-        return <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon}/>
-        </div>
-      })}
-    </div>
-  )
-}
+    <Suspense fallback={<div />}>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => {
+          return (
+            <div className="w-28 h-28" key={technology.name}>
+              <BallCanvas icon={technology.icon} />
+            </div>
+          );
+        })}
+      </div>
+    </Suspense>
+  );
+};
 
-export default SectionWrapper(Tech, '')
+export default SectionWrapper(Tech, "");
