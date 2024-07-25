8b375d96eb5aec383a2007d31119e7fa8b96fcea
SolisTenebrum <solistenebrum@icloud.com>
2024-07-25 14:56:03 +0500

    fixed hero accent colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,12 +9,12 @@ const Hero = () => {
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#2168ff]" />
+          <div className="w-5 h-5 rounded-full bg-[#568cff]" />
           <div className="w-1 sm:h-80 h-40 blue-gradient" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}`}>
-            Hi, I'm <span className="text-[#2168ff]">SolisTenebrum</span>
+            Hi, I'm <span className="text-[#568cff]">SolisTenebrum</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Frontend Developer
