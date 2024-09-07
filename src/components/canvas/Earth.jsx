ca018c70bc319d2762f9741583a26e5fec8b482a
SolisTenebrum <solistenebrum@icloud.com>
2024-09-07 22:29:27 +0500

    added custom scrollbar

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -18,7 +18,7 @@ const EarthCanvas = () => {
       frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
       camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
-      className="scrollCanvasAuto"
+      className="scrollCanvas"
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
