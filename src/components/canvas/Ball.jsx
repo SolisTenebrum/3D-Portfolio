dc66822c7d4bddf40b1e87f7569ae9a0ffc01bc3
SolisTenebrum <solistenebrum@icloud.com>
2024-09-08 21:33:34 +0500

    added rotation to inital position

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,5 +1,5 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
   Decal,
   Float,
@@ -9,11 +9,42 @@ import {
 } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+const Ball = ({ imgUrl, orbitRef, returnToInitialPosition }) => {
+  const [decal] = useTexture([imgUrl]);
+
+  useFrame(() => {
+    if (orbitRef.current && returnToInitialPosition) {
+      const currentRotationY = orbitRef.current.getAzimuthalAngle();
+      const targetRotationY = 0;
+      const delta = 0.05;
+
+      if (Math.abs(currentRotationY - targetRotationY) > 0.001) {
+        const newRotationY =
+          currentRotationY + (targetRotationY - currentRotationY) * delta;
+        orbitRef.current.setAzimuthalAngle(newRotationY);
+        orbitRef.current.update();
+      }
+
+      const currentRotationX = orbitRef.current.getPolarAngle();
+      const targetRotationX = Math.PI / 2;
+      const deltaX = 0.05;
+
+      if (Math.abs(currentRotationX - targetRotationX) > 0.001) {
+        const newRotationX =
+          currentRotationX + (targetRotationX - currentRotationX) * deltaX;
+        orbitRef.current.setPolarAngle(newRotationX);
+        orbitRef.current.update();
+      }
+    }
+  });
 
   return (
-    <Float speed={3} rotationIntensity={1} floatIntensity={2} floatingRange={[-0.2, 0.2]}>
+    <Float
+      speed={3}
+      rotationIntensity={1}
+      floatIntensity={2}
+      floatingRange={[-0.2, 0.2]}
+    >
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.5]} />
       <mesh castShadow receiveShadow scale={2.75}>
@@ -36,11 +67,40 @@ const Ball = (props) => {
 };
 
 const BallCanvas = ({ icon }) => {
+  const orbitRef = useRef();
+  const timeoutRef = useRef(null);
+  const [returnToInitialPosition, setReturnToInitialPosition] = useState(false);
+
+  const handleInteractionStart = () => {
+    setReturnToInitialPosition(false);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  };
+
+  const handleInteractionEnd = () => {
+    timeoutRef.current = setTimeout(() => {
+      setReturnToInitialPosition(true);
+    }, 500);
+  };
+
   return (
     <Canvas frameloop="always" gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} enablePan={false} />
-        <Ball imgUrl={icon} />
+        <OrbitControls
+          ref={orbitRef}
+          enableZoom={false}
+          enablePan={false}
+          enableDamping={true}
+          dampingFactor={0.1}
+          onStart={handleInteractionStart}
+          onEnd={handleInteractionEnd}
+        />
+        <Ball
+          imgUrl={icon}
+          orbitRef={orbitRef}
+          returnToInitialPosition={returnToInitialPosition}
+        />
       </Suspense>
 
       <Preload all />
